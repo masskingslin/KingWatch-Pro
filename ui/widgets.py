bd1513a2c386364bd6627b4d153745da7488ce98
Mass Kingslin <masskingslin@gmail.com>
2026-03-12 16:03:34 +0530

    Update widgets.py

--- ui/widgets.py
+++ ui/widgets.py
@@ -1,8 +1,11 @@
 from kivy.uix.boxlayout import BoxLayout
-from kivy.properties import StringProperty
+from kivy.properties import StringProperty, ColorProperty
+from kivy.utils import get_color_from_hex
 
 
 class InfoCard(BoxLayout):
-    icon  = StringProperty("●")
-    title = StringProperty("--")
-    value = StringProperty("...")
+    title       = StringProperty("--")
+    value       = StringProperty("...")
+    bg_color    = ColorProperty(get_color_from_hex("#1E1E1E"))
+    title_color = ColorProperty(get_color_from_hex("#FFFFFF"))
+    value_color = ColorProperty(get_color_from_hex("#00C853"))
