93383c1c660e7d321bbc75dd89dc2b9fce9b94e8
Mass Kingslin <masskingslin@gmail.com>
2026-03-12 06:49:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 from kivy.app import App
-from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
 
 class RootWidget(BoxLayout):
     pass
 
 class KingWatchApp(App):
-
     def build(self):
-        Builder.load_file("kingwatch.kv")
         return RootWidget()
 
 if __name__ == "__main__":
