d9e026faa5c2900d6bd36e8075c08f7c694f4e1a
Mass Kingslin <masskingslin@gmail.com>
2026-03-12 12:36:44 +0530

    Update cpu.py

--- core/cpu.py
+++ core/cpu.py
@@ -1,11 +1,21 @@
-import os
+_prev = None
 
-
-class CPUMonitor:
-
-    def get(self):
-        try:
-            load = os.getloadavg()[0]
-            return min(int(load * 10), 100)
-        except:
-            return 25
+def get_cpu():
+    global _prev
+    try:
+        with open("/proc/stat") as f:
+            line = f.readline()
+        vals = [int(x) for x in line.split()[1:]]
+        idle  = vals[3]
+        total = sum(vals)
+        if _prev is None:
+            _prev = (idle, total)
+            return 0.0
+        d_idle  = idle  - _prev[0]
+        d_total = total - _prev[1]
+        _prev = (idle, total)
+        if d_total == 0:
+            return 0.0
+        return round((1 - d_idle / d_total) * 100, 1)
+    except Exception:
+        return 0.0
