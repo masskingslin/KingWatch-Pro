e385125e93e865d4638a5f91d62277581ced05a2
Mass Kingslin <masskingslin@gmail.com>
2026-03-13 05:49:57 +0530

    Update widgets.py

--- ui/widgets.py
+++ ui/widgets.py
@@ -1,15 +1,13 @@
-import math
 from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import (
     StringProperty, ColorProperty,
-    NumericProperty, BooleanProperty, ListProperty
+    NumericProperty, BooleanProperty
 )
 from kivy.utils import get_color_from_hex
-from kivy.graphics import Color, Line, Ellipse, RoundedRectangle
 
 
 class StatCard(BoxLayout):
-    title        = StringProperty("--")
+    title        = StringProperty("")
     value        = StringProperty("...")
     subtitle     = StringProperty("")
     detail1      = StringProperty("")
@@ -22,16 +20,11 @@ class StatCard(BoxLayout):
     bar_color    = ColorProperty(get_color_from_hex("#00E676"))
     bar_bg       = ColorProperty(get_color_from_hex("#242424"))
     detail_color = ColorProperty(get_color_from_hex("#555555"))
-    spark_data   = ListProperty([])
-
-    def push_spark(self, value, max_pts=20):
-        pts = list(self.spark_data) + [float(value)]
-        self.spark_data = pts[-max_pts:]
 
 
 class ThemeChip(BoxLayout):
-    label        = StringProperty("Theme")
-    selected     = BooleanProperty(False)
-    chip_bg      = ColorProperty(get_color_from_hex("#1A1A1A"))
-    chip_border  = ColorProperty(get_color_from_hex("#333333"))
-    chip_text    = ColorProperty(get_color_from_hex("#888888"))
+    label       = StringProperty("Theme")
+    selected    = BooleanProperty(False)
+    chip_bg     = ColorProperty(get_color_from_hex("#1A1A1A"))
+    chip_border = ColorProperty(get_color_from_hex("#333333"))
+    chip_text   = ColorProperty(get_color_from_hex("#888888"))
