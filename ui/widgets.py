f546bb4120d243ca9245878b655d289d85dc93d5
Mass Kingslin <masskingslin@gmail.com>
2026-03-13 07:57:24 +0530

    Update widgets.py

--- ui/widgets.py
+++ ui/widgets.py
@@ -1,20 +1,30 @@
 from kivy.uix.boxlayout import BoxLayout
-from kivy.properties import StringProperty, NumericProperty
+from kivy.properties import (
+    StringProperty, ColorProperty,
+    NumericProperty, BooleanProperty
+)
+from kivy.utils import get_color_from_hex
 
 
-class CircularGauge(BoxLayout):
+class StatCard(BoxLayout):
+    title        = StringProperty("")
+    value        = StringProperty("...")
+    subtitle     = StringProperty("")
+    detail1      = StringProperty("")
+    detail2      = StringProperty("")
+    bar_pct      = NumericProperty(0)
+    bg_color     = ColorProperty(get_color_from_hex("#161616"))
+    title_color  = ColorProperty(get_color_from_hex("#555555"))
+    value_color  = ColorProperty(get_color_from_hex("#00E676"))
+    sub_color    = ColorProperty(get_color_from_hex("#555555"))
+    bar_color    = ColorProperty(get_color_from_hex("#00E676"))
+    bar_bg       = ColorProperty(get_color_from_hex("#242424"))
+    detail_color = ColorProperty(get_color_from_hex("#555555"))
 
-    label = StringProperty("")
-    value = StringProperty("")
-    percent = NumericProperty(0)
 
-
-class InfoCard(BoxLayout):
-
-    title = StringProperty("")
-    value = StringProperty("")
-    subtitle = StringProperty("")
-
-
-class MonitorWidget(BoxLayout):
-    pass
+class ThemeChip(BoxLayout):
+    label       = StringProperty("Theme")
+    selected    = BooleanProperty(False)
+    chip_bg     = ColorProperty(get_color_from_hex("#1A1A1A"))
+    chip_border = ColorProperty(get_color_from_hex("#333333"))
+    chip_text   = ColorProperty(get_color_from_hex("#888888"))
