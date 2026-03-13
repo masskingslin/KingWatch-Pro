c8cfc18afe172501bc81ddea270e3cf0c631b1e0
Mass Kingslin <masskingslin@gmail.com>
2026-03-13 19:05:31 +0530

    Update widgets.py

--- ui/widgets.py
+++ ui/widgets.py
@@ -1,9 +1,45 @@
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.image import Image as KivyImage
+from kivy.core.image import Image as CoreImage
 from kivy.properties import (
     StringProperty, ColorProperty,
     NumericProperty, BooleanProperty
 )
 from kivy.utils import get_color_from_hex
+import io
+
+from ui.gauge import draw_gauge, pct_to_color
+
+
+class GaugeImage(KivyImage):
+    """Kivy Image that renders a Pillow arc gauge."""
+
+    pct       = NumericProperty(0)
+    arc_color = ColorProperty(get_color_from_hex("#00E676"))
+    bg_color  = ColorProperty(get_color_from_hex("#282828"))
+
+    def __init__(self, **kw):
+        super().__init__(**kw)
+        self.nocache = True
+        self.allow_stretch = True
+        self.keep_ratio = True
+        self.bind(pct=self._redraw,
+                  arc_color=self._redraw,
+                  bg_color=self._redraw,
+                  size=self._redraw)
+
+    def _redraw(self, *a):
+        try:
+            fg = tuple(int(c * 255) for c in self.arc_color[:3]) + (255,)
+            bg = tuple(int(c * 255) for c in self.bg_color[:3])  + (255,)
+            sz = max(int(min(self.width, self.height)), 60)
+            png = draw_gauge(self.pct, size=sz, fg=fg, bg=bg,
+                             thick=max(sz // 10, 7))
+            buf = io.BytesIO(png)
+            ci  = CoreImage(buf, ext="png", nocache=True)
+            self.texture = ci.texture
+        except Exception:
+            pass
 
 
 class StatCard(BoxLayout):
