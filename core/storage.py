8d4415807a13f6faec540bba6a6b7bf79b1b7bea
Mass Kingslin <masskingslin@gmail.com>
2026-03-16 15:41:27 +0530

    Update storage.py

--- core/storage.py
+++ core/storage.py
@@ -1,17 +1,22 @@
 import os
 
+
 def get_storage():
 
-    s=os.statvfs("/")
+    try:
+        s = os.statvfs("/")
 
-    total=s.f_blocks*s.f_frsize
-    free=s.f_bavail*s.f_frsize
-    used=total-free
+        total = s.f_blocks * s.f_frsize
+        free = s.f_bavail * s.f_frsize
+        used = total - free
 
-    pct=(used/total)*100
+        pct = (used / total) * 100
 
-    return {
-        "pct":pct,
-        "used":f"{used//(1024**3)} GB",
-        "total":f"{total//(1024**3)} GB"
-    }
+        return {
+            "pct": pct,
+            "used": f"{used//(1024**3)}GB",
+            "total": f"{total//(1024**3)}GB"
+        }
+
+    except:
+        return {"pct": 0, "used": "0", "total": "0"}
